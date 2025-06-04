2027aa48d33ffd09cb2beeb2587858f7f45b253e
earth nut dev <earthnut.dev@outlook.com>
2025-06-04 10:34:55 +0800

    submit : 2025/6/4 10:34:55

--- src/dog.ts
+++ src/dog.ts
@@ -1,7 +1,10 @@
 import Dog from '@qqi/dev-log';
+import { isFalse } from 'a-type-of-js';
 
 /**  dev log  */
 export const dog = new Dog({
   name: 'jja',
   type: false,
 });
+/**  正式环境  */
+export const dun = isFalse(dog.type);
